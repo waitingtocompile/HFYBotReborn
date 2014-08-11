98573e797dcae0f16df72cb53ec5e7baa38883f9
waitingtocompile <git@edken.co.uk>
2014-08-11 21:54:44 +0100

    Project Files Added
    Due to Git derpiness visual studio project files were not included in
    the inital commit.

---
+++ HFYBot/ConsoleUtils.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HFYBot
+{
+    class ConsoleUtils
+    {
+        public static string readString(string descriptor, bool showChars)
+        {
+            Console.Write("\n" + descriptor + ": ");
+            string readString = "";
+            while (true)
+            {
+
+                ConsoleKeyInfo key = Console.ReadKey(true);
+                if (key.Key.Equals(ConsoleKey.Enter)) return readString;
+                else if (key.Key.Equals(ConsoleKey.Backspace) && readString.Length > 0)
+                {
+                    Console.Write("\b \b");
+                    readString = readString.Substring(0, readString.Length - 1);
+                }
+                else
+                {
+                    if (showChars)
+                        Console.Write(key.KeyChar);
+                    else
+                        Console.Write("*");
+                    readString += key.KeyChar;
+                }
+            }
+        }
+
+
+    }
+}
---
+++ HFYBot/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Security.Authentication;
+
+using RedditSharp;
+using RedditSharp.Things;
+
+namespace HFYBot
+{
+    class Program
+    {
+        public static Reddit redditInstance
+        {
+            private set;
+            get;
+        }
+        public static AuthenticatedUser user
+        {
+            private set;
+            get;
+        }
+        public static Subreddit sub
+        {
+            private set;
+            get;
+        }
+
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please input Reddit credentials (don't worry, I won't steal them)");
+            redditInstance = new Reddit();
+            attemptLogin();
+            Console.Clear();
+            Console.WriteLine("Login sucsessful, user has " + user.UnreadMessages.Count().ToString() + " unread messages");
+            sub = redditInstance.GetSubreddit("/r/MagnusTest");
+
+            foreach (Post post in sub.New.Take(25))
+            {
+                Console.WriteLine("{0}, by {1} with score of {2}", post.Title, post.AuthorName, post.Score.ToString());
+            }
+            Console.Write("Press Any Key...");
+            Console.Read();
+            
+        }
+
+        static void attemptLogin()
+        {
+            try
+            {
+                string name = ConsoleUtils.readString("Username", true);
+                string pass = ConsoleUtils.readString("Password", false);
+                user = redditInstance.LogIn(name, pass);
+            }
+            catch (AuthenticationException)
+            {
+                Console.Clear();
+                Console.WriteLine("Login credentials refused, please retry");
+                attemptLogin();
+            }
+        }
+    }
+}
