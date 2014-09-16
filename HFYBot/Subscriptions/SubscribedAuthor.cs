52d6e3d1df336491d7e11abded3726d03427cd1b
waitingtocompile <git@edken.co.uk>
2014-09-16 20:38:27 +0100

    Set up XML reading
    Basic, totally untested xml reading.

--- HFYBot/Subscriptions/SubscribedAuthor.cs
+++ HFYBot/Subscriptions/SubscribedAuthor.cs
@@ -3,26 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using System.Xml;
+
 
 namespace HFYBot.Subscriptions
 {
     //Class representing an author one or more users have subscibed to
     class SubscribedAuthor
     {
-        public const string SUBSCRIPTIONS_FILE_PATH = "subscriptions.xml";
-
-        XmlWriterSettings writerSettings = new XmlWriterSettings();
 
         public List<string> subscribers = new List<string>(0);
-        string author;
+        string author { public get; private set; }
 
 
         public SubscribedAuthor(string name)
         {
-            writerSettings = new XmlWriterSettings();
-            writerSettings.Indent = true;
-            writerSettings.WriteEndDocumentOnClose = true;
+            author = name;
         }
 
     }
--- HFYBot/Subscriptions/SubscriptionManager.cs
+++ HFYBot/Subscriptions/SubscriptionManager.cs
@@ -3,31 +3,82 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 
 namespace HFYBot.Subscriptions
 {
     //Deals with users directly asking the bot to do things. I know right, so needy!
-    class SubscriptionManager
+    static class SubscriptionManager
     {
+        public static readonly string subscriptionFile = "subscriptions.xml";
+
+        static SortedList<string, SubscribedAuthor> subscribedAuthors = new SortedList<string, SubscribedAuthor>(0);
+
         //Checks the bot's inbox for new mail/comments.
-        static void checkInbox()
+        public static void checkInbox()
         {
 
         }
 
+        public static void loadSubscriptionsFromFile()
+        {
+            if (System.IO.File.Exists(subscriptionFile))
+            {
+                System.IO.File.Create(subscriptionFile);
+                XmlWriterSettings set = new XmlWriterSettings();
+                XmlWriter writer = XmlWriter.Create(subscriptionFile, set);
+                writer.WriteStartDocument();
+                writer.WriteStartElement("Subscribtions");
+                writer.WriteStartElement("AuthorSubscriptions");
+                writer.WriteEndElement();
+                writer.WriteEndElement();
+                writer.WriteEndDocument();
+                writer.Flush();
+                writer.Close();
+            }
+            else
+            {
+                XmlDocument doc = new XmlDocument();
+                doc.LoadXml(subscriptionFile);
+                XmlNodeList authors = doc.SelectNodes("/Subscriptions/AuthorSubscriptions/Author");
+                foreach (XmlNode author in authors)
+                {
+                    string name = author.Attributes["name"].Value;
+                    SubscribedAuthor sub = new SubscribedAuthor(author.Attributes["name"].Value);
+                    subscribedAuthors.Add(name, sub);
+                    XmlNodeList subscribers = author.SelectNodes("/Subscriber");
+                    foreach (XmlNode subscriber in subscribers)
+                    {
+                        sub.subscribers.Add(subscriber.InnerText);
+                    }
+                }
+            }
+            
+        }
+
+        public static void addSubscriber(string author, string subscriber)
+        {
+
+        }
+
+        public static bool removeSubscriber(string author, string subscriber)
+        {
 
 
-            //System.IO.Directory.CreateDirectory("data");
-            //System.IO.File.Create("data\\hayo.xml");
-            //XmlWriterSettings set = new XmlWriterSettings();
-            //XmlWriter writer = XmlWriter.Create("data\\hayo.xml", set);
-            //writer.WriteStartDocument();
-            //writer.WriteStartElement("Test");
-            //writer.WriteAttributeString("name", "pancakes");
-            //writer.WriteElementString("topping", "battletoads");
-            //writer.WriteEndElement();
-            //writer.WriteEndDocument();
-            //writer.Flush();
-            //writer.Close();
+            return false;
+        }
+
+        public static string[] checkSubscribers(string author)
+        {
+
+            return null;
+        }
+
+        public static string[] checkSubscriptions(string user)
+        {
+
+            return null;
+        }
+            
     }
 }
