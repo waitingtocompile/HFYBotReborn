f406d3325fb075504fb9812a552a5154ff198e6d
waitingtocompile <git@edken.co.uk>
2014-09-22 21:46:21 +0100

    Remembered the commit button
    I am silly. All the XML stuff is there now. I'm sorry for neglecting you
    git.

--- HFYBot/Subscriptions/SubscriptionManager.cs
+++ HFYBot/Subscriptions/SubscriptionManager.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace HFYBot.Subscriptions
 {
@@ -14,15 +15,17 @@ namespace HFYBot.Subscriptions
 
         static SortedList<string, SubscribedAuthor> subscribedAuthors = new SortedList<string, SubscribedAuthor>(0);
 
+        static XmlDocument doc = new XmlDocument();
+
         //Checks the bot's inbox for new mail/comments.
         public static void checkInbox()
         {
-
+            
         }
 
         public static void loadSubscriptionsFromFile()
         {
-            if (System.IO.File.Exists(subscriptionFile))
+            if (!System.IO.File.Exists(subscriptionFile))
             {
                 System.IO.File.Create(subscriptionFile);
                 XmlWriterSettings set = new XmlWriterSettings();
@@ -38,8 +41,6 @@ namespace HFYBot.Subscriptions
             }
             else
             {
-                XmlDocument doc = new XmlDocument();
-                doc.LoadXml(subscriptionFile);
                 XmlNodeList authors = doc.SelectNodes("/Subscriptions/AuthorSubscriptions/Author");
                 foreach (XmlNode author in authors)
                 {
@@ -56,29 +57,99 @@ namespace HFYBot.Subscriptions
             
         }
 
-        public static void addSubscriber(string author, string subscriber)
-        {
-
+        static XmlNode getAuthorNode(string authorName){
+            foreach (XmlNode node in doc.SelectNodes("/Subscriptions/AuthorSubscriptions/Author")){
+                if(node.Attributes["name"].Value.Equals(authorName, StringComparison.InvariantCultureIgnoreCase))
+                    return node;
+            }
+            XmlNode listingNode = doc.SelectSingleNode("/Subscriptions/AuthorSubscriptions");
+            XmlElement newNode = doc.CreateElement("Author");
+            newNode.SetAttribute("name", authorName);
+            listingNode.AppendChild((XmlNode)newNode);
+            return (XmlNode)newNode;
         }
 
-        public static bool removeSubscriber(string author, string subscriber)
+        public static void addSubscriber(string authorName, string subscriber)
         {
+            SubscribedAuthor author;
+            XmlNode node = getAuthorNode(authorName);
+            if (!subscribedAuthors.TryGetValue(authorName, out author))
+            {
+                author = new SubscribedAuthor(authorName);
+                subscribedAuthors.Add(authorName, author);
+            }
+            author.subscribers.Add(subscriber);
+            XmlElement subscriberElement = doc.CreateElement("Subscriber");
+            subscriberElement.InnerText = subscriber;
+            node.AppendChild((XmlNode)subscriberElement);
+            doc.Save(subscriptionFile);
+        }
 
-
+        public static bool removeSubscriber(string authorName, string subscriber)
+        {
+            SubscribedAuthor author;
+            if (subscribedAuthors.TryGetValue("author", out author))
+            {
+                if (author.subscribers.Contains(subscriber))
+                {
+                    author.subscribers.Remove(subscriber);
+                    XmlNode authorNode = getAuthorNode(authorName);
+                    foreach (XmlNode SubscriberNode in authorNode.SelectNodes("/Subscriber"))
+                    {
+                        if (SubscriberNode.InnerText.Equals(subscriber, StringComparison.InvariantCultureIgnoreCase))
+                        {
+                            authorNode.RemoveChild(SubscriberNode);
+                            break;
+                        }
+                    }
+                    return true;
+                }
+            }
             return false;
         }
 
-        public static string[] checkSubscribers(string author)
+        public static List<string> checkSubscribers(string authorName)
         {
-
+            SubscribedAuthor author;
+            if (subscribedAuthors.TryGetValue(authorName, out author))
+            {
+                return author.subscribers;
+            }
             return null;
         }
 
-        public static string[] checkSubscriptions(string user)
+        public static List<string> checkSubscriptions(string user)
         {
-
-            return null;
+            List<string> subscriptions = new List<string>(0);
+            foreach (SubscribedAuthor author in subscribedAuthors.Values)
+            {
+                if(author.subscribers.Contains(user)) subscriptions.Add(author.author);
+            }
+            return subscriptions;
         }
-            
+
+
+
+        public static void Run()
+        {
+            doc.LoadXml(subscriptionFile);
+            loadSubscriptionsFromFile();
+        }
+    }
+
+
+    //Structure representing an author one or more users have subscibed to
+    private struct SubscribedAuthor
+    {
+
+        public List<string> subscribers = new List<string>(0);
+        public readonly string author;
+
+
+        public SubscribedAuthor(string name)
+        {
+            author = name;
+        }
+
     }
 }
